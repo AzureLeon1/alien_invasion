c4c6818edcac1034c2c49b80e59bb65b60f8d8fc
Leon <leonwangchn@163.com>
2018-08-23 09:39:31 +0800

    Bullet类 & 子弹设置

--- settings.py
+++ settings.py
@@ -11,9 +11,9 @@ class Settings():
         # 飞船的设置
         self.ship_speed_factor = 4.5
 
-        # # Bullet settings.
-        # self.bullet_speed_factor = 1
-        # self.bullet_width = 3
-        # self.bullet_height = 15
-        # self.bullet_color = 60, 60, 60
-        # self.bullets_allowed = 3
+        # 子弹设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
